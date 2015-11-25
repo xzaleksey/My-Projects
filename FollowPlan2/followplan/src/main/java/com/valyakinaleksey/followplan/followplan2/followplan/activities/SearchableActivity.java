b2eabc115a792c53a05e60ce0a309d9709f28c6e
xzaleksey <xaleksey@gmail.com>
2015-11-25 15:19:16 +0300

    clear

--- FollowPlan2/followplan/src/main/java/com/valyakinaleksey/followplan/followplan2/followplan/activities/SearchableActivity.java
+++
@@ -1,6 +0,0 @@
-package com.valyakinaleksey.followplan.followplan2.followplan.activities;
-
-import android.support.v7.app.AppCompatActivity;
-
-public class SearchableActivity extends AppCompatActivity {
-}
